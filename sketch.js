1b8944a50aaeec742c13204081c2522fa13a23e8
NoNelso <jasonr4nd@gmail.com>
2019-03-01 20:42:37 -0500

    created grid of triangle strips

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let c, r;
+let s = 25;
+let w = 600;
+let h = 600;
+
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(600, 600, WEBGL);
+  c = w / s;
+  r = h / s;
 }
 
 function draw() {
+  translate(-width / 2, -height / 2);
+  background(0);
+  stroke(200);
+  noFill();
+  for (let y = 0; y < r; ++y) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x <= c; ++x) {
+      vertex(x * s, y * s);
+      vertex(x * s, (y + 1) * s);
+    }
+    endShape();
+  }
 
 }
